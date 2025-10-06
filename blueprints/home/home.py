d2f928604625456575b19f0463f749fb7e3e2fd6
Pedro Ivo Mangelardo <mangelardo@gmail.com>
2025-10-06 16:35:17 -0300

    Melhorias na arquitetura do projeto. Melhor organização das rotas.

--- blueprints/home/home.py
+++ blueprints/home/home.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
 
-from utils import cadastro_ativo
+from utils import cadastro_ativo, perfis_permitidos
 
 
 home_bp = Blueprint('home', __name__)
@@ -13,6 +13,30 @@ home_bp = Blueprint('home', __name__)
 @cadastro_ativo
 def home(): 
     perfil = current_user.perfilAcesso
-    print(perfil)
     nome_usuario = current_user.nome
     return render_template("home.html", nome_usuario=nome_usuario, perfil=perfil)
+
+@home_bp.route("/cadastros")
+@login_required
+@cadastro_ativo
+def cadastros(): 
+    perfil = current_user.perfilAcesso
+    nome_usuario = current_user.nome
+    return render_template("cadastros.html", nome_usuario=nome_usuario, perfil=perfil)
+
+@home_bp.route("/rotas")
+@login_required
+@cadastro_ativo
+def rotas(): 
+    perfil = current_user.perfilAcesso
+    nome_usuario = current_user.nome
+    return render_template("rotas.html", nome_usuario=nome_usuario, perfil=perfil)
+
+@home_bp.route("/recursoshumanos")
+@login_required
+@cadastro_ativo
+@perfis_permitidos(['Admin', 'Motorista'])
+def recursoshumanos(): 
+    perfil = current_user.perfilAcesso
+    nome_usuario = current_user.nome
+    return render_template("recursoshumanos.html", nome_usuario=nome_usuario, perfil=perfil)
